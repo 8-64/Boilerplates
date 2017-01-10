11c8593ba6f1e5f5bc7c4c9a9ce02d28ce6a3444
Vasyl <basilwashere@gmail.com>
2017-01-10 17:52:51 +0200

    Custom attributes handling

--- Perl/Module/Basic.pm
+++ Perl/Module/Basic.pm
@@ -7,6 +7,9 @@ use warnings;
 # Module version
 our $VERSION = v0.1;
 
+# Some inheritance, set by hand
+our (@ISA) = ('UNIVERSAL');
+
 # Will be called when "use Module;" is used
 sub import {
     my ($self, @args) = @_;
@@ -38,11 +41,9 @@ sub AUTOLOAD {
     my $method = $AUTOLOAD;
     $method    =~ s/.*:://;
 
-    my $package = __PACKAGE__;
-
-    my ($object, @args) = (ref $_[0] eq $package) ?
-                                               @_ :
-                                          ('', @_);
+    my ($object, @args) = (ref $_[0] eq __PACKAGE__) ?
+                                                  @_ :
+                                             ('', @_);
 
     print qq[Called undeclared method "$method" on object "$object" with arguments "@args"\n];
 }
@@ -53,12 +54,37 @@ sub DESTROY {
     print qq[Object "$self" is about to be garbage collected.\n];
 }
 
+# ATTRIBUTES HANDLING
+# Custom attributes should have at least one CaPiTaL letter in them in order
+# to distinguish from any possible future reserved word
+# Could be named UNIVERSAL::MODIFY_<TYPE>_ATTRIBUTES to apply for everything
+# Types: "SCALAR", "ARRAY", "HASH", "CODE", (uc reftype $self)
+sub UNIVERSAL::MODIFY_SCALAR_ATTRIBUTES {
+    my ($class, $self, @attributes) = @_;
+    my @bad_attributes = ();
+
+    print "Attributes [@attributes] for item [$self] of class [$class] are processed.\n";
+
+    # Must return an empty list if everything is OK, otherwise - list of bad
+    # attributes 
+    @bad_attributes;
+}
+
 1;
 
 __END__
 
-# Maybe put more rare boilerplates of predefined methods here?
-#
+More rare boilerplates of predefined methods:
+
+# Fetch attributes
+# Invoked via "attributes::get($self);"
+sub UNIVERSAL::FETCH_SCALAR_ATTRIBUTES {
+    my ($class, $self) = @_;
+    my (@list_of_attributes) = ();
+
+    # Should return list of associated attributes
+    @list_of_attributes;
+}
 
 =pod
 
@@ -66,7 +92,7 @@ __END__
 
 =head1 NAME
 
-Module::Basic - short module desciption after hyphen
+Module::Basic - short module description after hyphen
 
 =head1 VERSION
 
