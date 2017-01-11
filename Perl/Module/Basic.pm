b872a7628eb9c79407121129d96a71f91444ca2c
Vasyl <basilwashere@gmail.com>
2017-01-11 14:07:44 +0200

    Added Module::Basic::INC method

--- Perl/Module/Basic.pm
+++ Perl/Module/Basic.pm
@@ -8,7 +8,7 @@ use warnings;
 our $VERSION = v0.1;
 
 # Some inheritance, set by hand
-our (@ISA) = ('UNIVERSAL');
+our (@ISA) = ('Module::Any');
 
 # Will be called when "use Module;" is used
 sub import {
@@ -35,6 +35,33 @@ sub new {
     bless(\%data => $class);
 }
 
+# Custom handling of entries in @INC array
+# Will be called if nothing is found using entries in @INC before an object of 
+# this class
+sub Module::Basic::INC {
+    my ($self, $module) = @_;
+
+    print "Resolving module [$module], called on object [$self]\n";
+
+    # This method should return either nothing, or list of up to four next
+    # values:
+    # 1) A reference to a scalar, containing any initial source code to prepend
+    # to the file or generator output.
+    # 2) A filehandle, from which the file will be read.
+    # 3) A reference to a subroutine. If there is no filehandle (previous
+    # item), then this subroutine is expected to generate one line of source
+    # code per call, writing the line into $_ and returning 1, then finally at
+    # end of file returning 0. If there is a filehandle, then the subroutine
+    # will be called to act as a simple source filter, with the line as read in
+    # $_ . Again, return 1 for each valid line, and 0 after all lines have been
+    # returned.
+    # 4) Optional state for the subroutine. The state is passed in as $_[1] .
+    # A reference to the subroutine itself is passed in as $_[0] .
+    return (
+        \"print qq[Resolved inclusion of a module ($module).\n];"
+    );
+}
+
 # Autoloading undeclared methods
 our $AUTOLOAD;
 sub AUTOLOAD {
@@ -46,6 +73,9 @@ sub AUTOLOAD {
                                              ('', @_);
 
     print qq[Called undeclared method "$method" on object "$object" with arguments "@args"\n];
+
+    # If a new method was auto-generated, it is often a good idea to jump
+    # to it, erasing this step in the call stack, using "goto &sub;" form. 
 }
 
 sub DESTROY {
@@ -57,8 +87,9 @@ sub DESTROY {
 # ATTRIBUTES HANDLING
 # Custom attributes should have at least one CaPiTaL letter in them in order
 # to distinguish from any possible future reserved word
-# Could be named UNIVERSAL::MODIFY_<TYPE>_ATTRIBUTES to apply for everything
-# Types: "SCALAR", "ARRAY", "HASH", "CODE", (uc reftype $self)
+# Could be named UNIVERSAL::MODIFY_<TYPE>_ATTRIBUTES to apply for attributes
+# of the particular data type
+# Data types: "SCALAR", "ARRAY", "HASH", "CODE", (uc reftype $self)
 sub UNIVERSAL::MODIFY_SCALAR_ATTRIBUTES {
     my ($class, $self, @attributes) = @_;
     my @bad_attributes = ();
@@ -69,6 +100,7 @@ sub UNIVERSAL::MODIFY_SCALAR_ATTRIBUTES {
     # attributes 
     @bad_attributes;
 }
+
 
 1;
 
